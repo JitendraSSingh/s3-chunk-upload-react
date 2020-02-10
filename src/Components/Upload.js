f36a93ade8b27b4cab8642cdcd3ffa1171d49430
Jitu <jitu@tenancy.co.nz>
2020-02-10 21:58:57 +1300

    Test Server enpoint change

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -13,14 +13,14 @@ const uploader = new FineUploaderS3({
     },
     deleteFile: {
       enabled: true,
-      endpoint: 'http://localhost:8080/uploads'
+      endpoint: 'http://fileupload.nanufeed.com/uploads'
     },
     request: {
       endpoint: "https://247stream.s3.fr-par.scw.cloud",
       accessKey: "SCW6WSFJBDYT5CQWFD9T"//this is public so don't worry
     },
     signature: {
-      endpoint: "http://localhost:8080/sign-policy",
+      endpoint: "http://fileupload.nanufeed.com/sign-policy",
       version: 4
     },
     objectProperties: {
